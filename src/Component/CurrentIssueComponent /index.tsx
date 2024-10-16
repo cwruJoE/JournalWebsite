6ee55af14bc3dc796e8f43743d9919aa8496b3b3
Shourya Poddar <shouryavpoddar@gmail.com>
2024-10-15 22:17:02 -0400

    Fixed UI on the currentIssueComponent

--- src/Component/CurrentIssueComponent /index.tsx
+++ src/Component/CurrentIssueComponent /index.tsx
@@ -5,21 +5,19 @@ import JournalImage from '../../assets/images/JournalCoverPage.png'; // Update t
 
 const CurrentJournalIssue = () => {
     return (
-<div className="container mx-auto mt-8">
-      {/* Title */}
-      <h1 className="text-4xl font-marcellus text-center mb-8">Current Journal Issue</h1>
+        <div className="mx-auto mt-8">
+            {/* Title */}
+            <h1 className="text-4xl font-marcellus mb-8 border-b text-start border-black">Current Journal Issue</h1>
 
-      {/* Journal Cover */}
-      <div className="flex justify-center items-center">
-        <div className="w-full md:w-1/2">
-          <img
-            src={JournalImage}
-            alt="CWRU Journal of Economics"
-            className="w-full h-auto border-2 border-gray-400"
-          />
+            {/* Journal Cover */}
+            <div className="w-full">
+                <img
+                    src={JournalImage}
+                    alt="CWRU Journal of Economics"
+                    className="w-full h-auto border-2 border-gray-400"
+                />
+            </div>
         </div>
-      </div>
-    </div>
     )
 };
 
